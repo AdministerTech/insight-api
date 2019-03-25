cdc6d9dc5c4709d82a7caa9f5e45005fe2b67134
Jayson <j4ys0n@gmail.com>
2019-03-25 11:38:54 -0600

    get mn list in service

--- services/MasternodeService.js
+++ services/MasternodeService.js
@@ -6,31 +6,63 @@ function MasternodeService(options) {
 }
 
 MasternodeService.prototype.start = function(next) {
-  var self = this;
+  let self = this;
 
-  this.common.log.info('[MasternodeService] Start...');
+  self.common.log.info('[MasternodeService] Start...');
+  self.tryGetList();
 
-  return next()
-}
+  return next();
+};
+
+MasternodeService.prototype.tryGetList = function () {
+  let self = this;
+  let list = [];
+  const listPoll = setInterval(function() {
+    const mnListHandler = function(error, result) {
+      if (error) {
+        this.common.log.info('[MasternodeService] error' + error);
+      } else {
+        if (result.length > 0) {
+          list = result;
+          this.common.log.info('[MasternodeService] got mn list. length: ' + list.length);
+          clearInterval(listPoll);
+        }
+      }
+    }
+    this.common.log.info('[MasternodeService] trying to get mn list');
+    self.getMNList(mnListHandler);
+  }, 5000);
+};
 
 MasternodeService.prototype.updateMasternodeBasics = function() {
   // get
-}
+
+};
 
 MasternodeService.prototype.updateMasternodeLastSeen = function() {
 
-}
+};
 
 MasternodeService.prototype.updateMasternodeP2P = function() {
 
-}
+};
 
 MasternodeService.prototype.updateMasternodeBalance = function() {
 
-}
+};
 
 MasternodeService.prototype.updateMasternodeLastPaid = function () {
 
-}
+};
+
+MasternodeService.prototype.getMNList = function(callback) {
+	this.node.services.ravend.getMNList(function(err, result){
+		let MNList = result || [];
+		if (err) {
+			return callback(err);
+		}
+		callback(null,MNList);
+	});
+};
 
 module.exports = MasternodeService;
